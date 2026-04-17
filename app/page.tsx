85bbdca2d4c4879bce2fe8383054f37fc4c01113
Augusto215 <augusto.dataanalysis@gmail.com>
2026-04-17 10:24:02 -0300

    17/04

--- app/page.tsx
+++ app/page.tsx
@@ -30,13 +30,13 @@ export default function Home() {
           <ProfileCard />
 
           <SubscribeBox
-            onSubscribe={() => openModal("1 Mês", 2)}
-            onLogin={() => openModal("1 Mês", 2)}
+            onSubscribe={() => openModal("1 Mês", 13.87)}
+            onLogin={() => openModal("1 Mês", 13.87)}
           />
 
           <PlansSection onSelect={(label, amount) => openModal(label, amount)} />
 
-          <ContentFeed onLockedClick={() => openModal("1 Mês", 2)} />
+          <ContentFeed onLockedClick={() => openModal("1 Mês", 13.87)} />
         </div>
       </main>
 
